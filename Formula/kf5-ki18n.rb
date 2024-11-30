df559ce952f93b6e7d7f0414a830c0cb44fe8db2
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-11-30 17:52:53 +1100

    I am the master of screwing up with double quotes

--- Formula/kf5-ki18n.rb
+++ Formula/kf5-ki18n.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Kf5Ki18n < Formula
   desc "Text internationiser for KDE"
-  homepage "https://api.kde.org/frameworks/ki18n/html/index.html""
+  homepage "https://api.kde.org/frameworks/ki18n/html/index.html"
   url "https://download.kde.org/stable/frameworks/5.111/ki18n-5.111.0.tar.xz"
   sha256 "4d0a27bdc89c9111888930ab17cb1f8416450f0baceffb6d52571e135c85df27"
   head "https://invent.kde.org/frameworks/ki18n.git", branch: "master"
