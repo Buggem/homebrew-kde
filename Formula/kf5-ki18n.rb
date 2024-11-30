a303bf4da8f2aaefff2ecd3f716fe30f517a6b7e
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-11-30 18:03:11 +1100

    Fix

--- Formula/kf5-ki18n.rb
+++ Formula/kf5-ki18n.rb
@@ -1,7 +1,7 @@
-require "formula"
+require_relative "../lib/cmake"
 
 class Kf5Ki18n < Formula
-  desc "Text internationiser for KDE"
+  desc "Text internationaliser for KDE"
   homepage "https://api.kde.org/frameworks/ki18n/html/index.html"
   url "https://download.kde.org/stable/frameworks/5.111/ki18n-5.111.0.tar.xz"
   sha256 "4d0a27bdc89c9111888930ab17cb1f8416450f0baceffb6d52571e135c85df27"
